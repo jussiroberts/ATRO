47f9d60048175d6a99c6dd39c53e9fc39cbddce8
joniniss <jojuniss@gmail.com>
2015-03-04 10:03:59 +0200

    Cleanup

--- atro/pipelines.py
+++ atro/pipelines.py
@@ -8,34 +8,23 @@ import psycopg2
 
 class AtroPipeline(object):
     def process_item(self, item, spider):
-    	#with open('items', 'a') as f:
-    		#f.write('title: {0}, author: {1}\n'.format(item['title'], item['author']))
-    	try:
+        #with open('items', 'a') as f:
+            #f.write('title: {0}, author: {1}\n'.format(item['title'], item['author']))
+        try:
             conn = psycopg2.connect("dbname='jonitestdb' user='jussi' host='localhost' password='helevetti'")
         except:
             print "Failed to establish connection to database."
         cur = conn.cursor()
 
         for t in item['title']:
-            #cur.execute("INSERT INTO publication (name) VALUES (%s);", (item['title'],))
             cur.execute("INSERT INTO publication (name) VALUES (%s);", (t,))
             conn.commit()
-                #SELECT ID???
 
         for a in item['author']:
-        #cur.execute("INSERT INTO author (name) VALUES (%s);", (item['author'],))
             cur.execute("INSERT INTO author (name) VALUES (%s);", (a,))
-            #conn.commit()
-                #cur.execute("INSERT INTO author_publication (author_id, pub_id) VALUES (%s, %s);", ())
             cur.execute("INSERT INTO author_publication (author_id, pub_id) SELECT author_id, pub_id FROM author, publication WHERE author.name = (%s) AND publication.name = (%s);", (a, t))
-            conn.commit()
-            #for j in journal:
-             #   cur.execute("UPDATE testpublication SET journal = %s WHERE otsikko = %s;", (j, o)) 
-              #  conn.commit()
-            #for a in abstract:
-             #   cur.execute("UPDATE testpublication SET abstract = %s WHERE otsikko = %s;", (a, o))
-              #  conn.commit()
+            conn.commit()      
 
         cur.close()
-        conn.close()	
+        conn.close()    
         return item
--- atro/spiders/spider_atro.py
+++ atro/spiders/spider_atro.py
@@ -10,21 +10,16 @@ from selenium import webdriver
 from pyvirtualdisplay import Display #only needed on the RaspberryPi
 import lxml.html
 from lxml import etree
-#from selenium.webdriver.common.by import By
 import time
 import psycopg2
 
-#Joonan testi jutska yhdys sana
-#markorepairs
-#MIIKKAtest
-#testcomment
-#jokujuttunen
 class AtroSpider(scrapy.Spider):
     name = "atrobot"
     allowed_domains = ["www.ncbi.nlm.nih.gov"]
     
     start_urls = [
-    "http://www.ncbi.nlm.nih.gov/pubmed?term=als"
+    #"http://www.ncbi.nlm.nih.gov/pubmed?term=als"
+    "http://www.ncbi.nlm.nih.gov/pubmed/?term=Zhang+X"
     ]
 
     extractor = SgmlLinkExtractor()
@@ -39,13 +34,9 @@ class AtroSpider(scrapy.Spider):
         #self.driver.dispose()
 
     def parse(self, response):
-        #filename = "atro_urls1.txt"
-        
         base_url = 'http://www.ncbi.nlm.nih.gov'
-
         wdr = self.driver
         wdr.get(response.url)
-
 
         #---PUBMED PAGE SETTINGS--- 
         #Sets the amount of results per page to 5 (for testing purposes), 'ps200' for 200 per page     
@@ -54,7 +45,7 @@ class AtroSpider(scrapy.Spider):
         dsettings.click()
         pagesetting = wdr.find_element_by_id('ps5')
         pagesetting.click()
-       
+        wdr.implicitly_wait(1)
         
         #---GET PUBLICATION LINKS, NUMBER OF PAGES AND CURRENT PAGE FOR THE FIRST PAGE---
         html = wdr.page_source
@@ -71,29 +62,31 @@ class AtroSpider(scrapy.Spider):
         hrefs.extend(links)
         pages = hxs.xpath('//*[@class="num"]/@last').extract()[0] #
 
-        count = 0
-        print count, current, pages
-        print len(hrefs)
+        #count = 0
+        print ("Current page: {0}, Pages total: {1}".format(current, pages))
+        print ("{} publication URLs saved".format(len(hrefs)))
 #int(pages)
 
         #---LOOP THROUGH THE RESULT PAGES, GETTING THE LINKS OF THE PUBLICATIONS---
         #---CRAWLS ONLY TWO RESULT PAGES FOR NOW---
-        while int(current) < 1:
-            count = count + 1
-            print '....\n'
-            print count, current, pages
+        while int(current) < 2:
+            #count = count + 1
+     
 
             #---GO TO THE NEXT PAGE AND GET ALL THE PUBLICATION LINKS FROM THERE
             try:
                 next = wdr.find_element_by_link_text('Next >')
                 next.click()
-                wdr.implicitly_wait(3)
+                wdr.implicitly_wait(0.5)
                 html = wdr.page_source
                 root = lxml.html.fromstring(html)
                 links = root.xpath('//*[@class="title"]/a/@href')
                 current = root.xpath('//*[@class="num"]/@value')[0]
                 hrefs.extend(links)
-                print len(hrefs)
+                print '....\n'
+                print ("Current page: {0}, Pages total: {1}".format(current, pages))
+                print ("{} publication URLs saved".format(len(hrefs)))
+
 
             except Exception as ex:
                 print ex
@@ -109,7 +102,6 @@ class AtroSpider(scrapy.Spider):
     def parse_publication(self, response):
         status = response.status
         url = response.url
-        #filename = "atro1.txt"
 
         if status == 200:
             hxs = Selector(response)
@@ -120,38 +112,7 @@ class AtroSpider(scrapy.Spider):
 
         else:
             yield Request(url, self.parse_publication)
-"""
-            #otsikko = hxs.select('//*[@class="content-title"]/text()').extract()
-            otsikko = hxs.xpath('//div[@class="rprt abstract"]/h1/text()').extract()
-            author = hxs.xpath('//div[@class="auths"]/a/text()').extract()
-            journal = hxs.xpath('//div[@class="cit"]/a/@title').extract()
-            abstract = hxs.xpath('//div[@class="abstr"]//p/abstracttext/text()').extract()
-			
-            try:
-                conn = psycopg2.connect("dbname='jonitestdb' user='jussi' host='localhost' password='helevetti'")
-            except:
-                print "Failed to establish connection to database."
-            cur = conn.cursor()
 
-            for o in otsikko:
-                cur.execute("INSERT INTO publication (name) VALUES (%s);", (o,))
-                conn.commit()
-                #SELECT ID???
-
-            for a in author:
-                cur.execute("INSERT INTO author (name) VALUES (%s);", (a,))
-                #cur.execute("INSERT INTO author_publication (author_id, pub_id) VALUES (%s, %s);", ())
-                conn.commit()
-            #for j in journal:
-             #   cur.execute("UPDATE testpublication SET journal = %s WHERE otsikko = %s;", (j, o)) 
-              #  conn.commit()
-            #for a in abstract:
-             #   cur.execute("UPDATE testpublication SET abstract = %s WHERE otsikko = %s;", (a, o))
-              #  conn.commit()
-
-            cur.close()
-            conn.close()
-"""
        
 
 
