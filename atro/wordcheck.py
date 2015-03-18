4f7dba598390ea93c8e8c50f97cfc572c1ddb3ac
Jootoiva <joonatoivanen12@gmail.com>
2015-03-18 10:38:31 +0200

    Created wordcheck.py
    Doesn't work yet

---
+++ atro/wordcheck.py
@@ -0,0 +1,16 @@
+
+class Wordcheck():
+
+	def checkwords(abstract):
+
+		searchwordlist = ["ALS", "copper"]
+		found_search_word = 0
+
+		#
+		for x in item['abstract']:
+			if (//x[contains(text(), searchwordlist)]):
+				print jee
+
+
+
+
