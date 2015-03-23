1df72d49f2a8bee0b872b33fe40bb44ba0dfaa5e
jussiroberts <jussi.roberts@gmail.com>
2015-03-23 15:39:55 +0200

    testing

--- atro/wordcheck.py
+++ atro/wordcheck.py
@@ -5,37 +5,40 @@ class Wordcheck():
 	#searchwords = ['banana', 'apple', 'dummy']
 	#wordcount = 0
 
-	@staticmethod
-	def check(abstract):
-		my_regex = 'null'
-		wordcount = 0
-		abstractlist = abstract.split()
-		
-		
-		searchwords = ['cancer', 'proximal humerus', 'extrahepatic biliary duct']	
-		for sword in searchwords:
-			for word in abstractlist:
-				my_regex = r"\b(?=\w)" + re.escape(sword) + r"\b(?!\w)"
-				nextwordindex = abstractlist.index(word)+1
-				nextword = abstractlist[nextwordindex]
-				nextnextword = abstractlist[nextwordindex+1]
-				if (" ") in sword:
-					if re.search(my_regex, word+' '+nextword+' '+nextnextword, re.IGNORECASE):
-						wordcount = wordcount + 1
-						with open('foundsearchwords.txt', 'a') as f:
-							f.write('search word: {0}\n'.format(sword.encode('utf8'),))
-							f.write('word in abstract: {0}\n'.format(word.encode('utf8'),))
-							f.write('words found: {0}\n'.format(wordcount,))
-						break
+    @staticmethod
+    def check(abstract):
+        my_regex = 'null'
+        wordcount = 0
+        abstractlist = abstract.split()
+        f = open('searchwords_list.txt', 'r')
+        searchwords = f.readlines()
+        f = close()
+        with open('kokkeilu.txt', 'a') as f:
+            for s in searchwords:
+                f.write('%s\n' % s)
 
-				if re.search(my_regex, word, re.IGNORECASE):
+		#searchwords = ['cancer', 'proximal humerus', 'extrahepatic biliary duct']	
+        for sword in searchwords:
+            for word in abstractlist:
+                my_regex = r"\b(?=\w)" + re.escape(sword) + r"\b(?!\w)"
+                nextwordindex = abstractlist.index(word)+1
+                nextword = abstractlist[nextwordindex]
+                nextnextword = abstractlist[nextwordindex+1]
+                if (" ") in sword:
+                    if re.search(my_regex, word+' '+nextword+' '+nextnextword, re.IGNORECASE):
+                        wordcount = wordcount + 1
+                        with open('foundsearchwords.txt', 'a') as f:
+                            f.write('search word: {0}\n'.format(sword.encode('utf8'),))
+                            f.write('word in abstract: {0}\n'.format(word.encode('utf8'),))
+                            f.write('words found: {0}\n'.format(wordcount,))
+                        break
 
-					wordcount = wordcount + 1
-					with open('foundsearchwords.txt', 'a') as f:
-						f.write('search word: {0}\n'.format(sword.encode('utf8'),))
-						f.write('word in abstract: {0}\n'.format(word.encode('utf8'),))
-						f.write('words found: {0}\n'.format(wordcount,))
-					break
-		return wordcount
-				
-				
+                if re.search(my_regex, word, re.IGNORECASE):
+
+                    wordcount = wordcount + 1
+                    with open('foundsearchwords.txt', 'a') as f:
+                        f.write('search word: {0}\n'.format(sword.encode('utf8'),))
+                        f.write('word in abstract: {0}\n'.format(word.encode('utf8'),))
+                        f.write('words found: {0}\n'.format(wordcount,))
+                    break
+        return wordcount
