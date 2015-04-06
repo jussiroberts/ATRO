46044dd9226a926bf9111243e96962a8422c70e0
joniniss <jojuniss@gmail.com>
2015-04-06 11:22:01 +0300

    Added some quick and dirty testcases for pipelines and wordcheck. Refactoring of pipelines

--- atro/pipelines.py
+++ atro/pipelines.py
@@ -13,85 +13,96 @@ from dbconn import Dbconn
 
 #The pipeline processes all items that are yielded by the spider. Each item contains relevant metadata for one publication.
 class AtroPipeline(object):
-    def process_item(self, item, spider):
-        db = Dbconn()
-        w1 = Wordcheck()
-        publication_rank = 0
-        doi = "NULL"
-        pii = "NULL"
-        date_crawled = "NULL"
-        author_list = []
-        journal = "NULL"
-        title = "NULL"
-        found_searchwords = []
 
+    def process_title(self, title):
         #Remove the dot in the end of the title   
         try:
-            if item['title'][0].endswith('.'):
-                t = item['title'][0][:-1]
-                title = t
-            else:
-                title = t
+            if title.endswith('.'):
+                title = title[:-1]
         except:
             print "title error"
-                        
-        for a in item['author']:
+        return title
+    
+    def process_authors(self, authorslist):
+        author_list = []
+        for a in authorslist:
             if a not in author_list:
                 try:
                     author_list.append(a)
                 except:
                     print "author error"
+        return author_list
 
+    def process_otherinfo(self, otherinfo):
         #Get the year of publication, doi, pii and journal info
-        for p in item['otherinfo']:
-            try:    
-                otherinfostr = str(p.encode('utf8')) 
-                otherinfolist = otherinfostr.split(" ")
-                otherinfolist2 = otherinfostr.split(".")
-                year_of_publication = otherinfolist2[1][:5]
-                journal = otherinfolist2[0]
-            except:
-                print "otherinfo error"
-               
-            #Check whether the publication has a doi or not
-            try:      
-                index = otherinfolist.index('doi:') + 1
-                if otherinfolist[index].endswith('.'):
-                    doi = otherinfolist[index][:-1]   
-                else:
-                    doi = otherinfolist[index]  
-            except:
-                print "no DOI"
+        doi = "NULL"
+        pii = "NULL"
+        year_of_publication = "NULL"
+        journal = "NULL"
+        try:    
+            otherinfolist = otherinfo.split(" ")
+            otherinfolist2 = otherinfo.split(".")
+            year_of_publication = otherinfolist2[1][:5]
+            year_of_publication = year_of_publication[1:]
+            journal = otherinfolist2[0]
+        except:
+            print "otherinfo error"
+           
+        #Check whether the publication has a doi or not
+        try:      
+            index = otherinfolist.index('doi:') + 1
+            if otherinfolist[index].endswith('.'):
+                doi = otherinfolist[index][:-1]   
+            else:
+                doi = otherinfolist[index]  
+        except:
+            print "no DOI"
 
-            #incomplete code
-            try:
-                index = otherinfolist.index('pii:') + 1
-                if otherinfolist[index].endswith('.'):
-                    pii = otherinfolist[index][:-1]
-                    print pii
-                    print "-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-"
-                elif otherinfolist[index].endswith('doi:'):
-                    pii = otherinfolist[index][:-1]
-                    print pii
-                    print "**********************************************************************"
-                else:
-                    pii = otherinfolist[index]
-            except:
-                print "no PII"
+        #incomplete code
+        try:
+            index = otherinfolist.index('pii:') + 1
+            if otherinfolist[index].endswith('.'):
+                pii = otherinfolist[index][:-1]
+                print pii
+                print "-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-"
+            elif otherinfolist[index].endswith('doi:'):
+                pii = otherinfolist[index][:-1]
+                print pii
+                print "**********************************************************************"
+            else:
+                pii = otherinfolist[index]
+        except:
+            print "no PII"
 
-         
+        return year_of_publication, journal, doi 
+        #and pii
+        
+    def process_abstract(self, abstractlist): 
         #Append abstract groups together
         ab = ''
         try:
-            for b in item['abstract']:       
+            for b in abstractlist:       
                 ab += str(b.encode('utf8'))+' ' 
             abstract = ab[:-1].strip()
         except:
             abstract = "ERROR"
             print "abstract error"
+        return abstract
 
+    def process_item(self, item, spider):
+        db = Dbconn()
+        w1 = Wordcheck()
+        publication_rank = 0
+        date_crawled = "NULL"
+        author_list = []
+        title = "NULL"
+        found_searchwords = []
 
-        #Search for relevant keywords in the abstract
+        title = self.process_title(item['title'][0])
+        author_list = self.process_authors(item['author'])
+        abstract = self.process_abstract(item['abstract'])
+        year_of_publication, journal, doi = self.process_otherinfo(item['otherinfo'][0])
+          #Search for relevant keywords in the abstract
         try:
 
             publication_rank, found_searchwords = w1.check(abstract)        
@@ -107,3 +118,7 @@ class AtroPipeline(object):
         if publication_rank > 0:
             db.insert_publication(metadata)
         return item
+
+    
+
+      
